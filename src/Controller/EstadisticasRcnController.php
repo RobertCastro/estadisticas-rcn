95817c5eda26c07d2e544036f6847d779297ea3a
Robert Castro <oneroberth@gmail.com>
2023-11-07 20:59:32 -0500

    first commit

---
+++ src/Controller/EstadisticasRcnController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\estadisticas_rcn\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Returns responses for Estadísticas RCN routes.
+ */
+class EstadisticasRcnController extends ControllerBase {
+
+  /**
+   * Builds the response.
+   */
+  public function build() {
+
+    $build['content'] = [
+      '#type' => 'item',
+      '#markup' => $this->t('It works!'),
+    ];
+
+    return $build;
+  }
+
+}
