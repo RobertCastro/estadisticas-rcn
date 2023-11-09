5e8f953c30b7050daa8316e291f9e1e5dae62d30
Robert Castro <oneroberth@gmail.com>
2023-11-09 12:08:13 -0500

    Filtrado y consulta

--- src/Controller/EstadisticasRcnController.php
+++ src/Controller/EstadisticasRcnController.php
@@ -3,6 +3,7 @@
 namespace Drupal\estadisticas_rcn\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
 
 /**
  * Returns responses for Estadísticas RCN routes.
@@ -23,8 +24,56 @@ class EstadisticasRcnController extends ControllerBase {
   }
 
   public function content() {
-    // Return the form.
     return \Drupal::formBuilder()->getForm('Drupal\estadisticas_rcn\Form\EstadisticasRcnFilterForm');
+  }
+
+  /**
+   * Retorna los contenidos creados entre las fechas configuradas.
+   */
+  public function contenidosPorFecha() {
+
+    $build = [];
+    // Incluir el formulario de fechas
+    $build['form'] = \Drupal::formBuilder()->getForm('Drupal\estadisticas_rcn\Form\EstadisticasRcnFilterForm');
+
+    $configFilter = $this->config('estadisticas_rcn.settings');
+    $fecha_inicial = $configFilter->get('fecha_inicial');
+    $fecha_final = $configFilter->get('fecha_final');
+
+    // Convertir las fechas a formato UNIX timestamp.
+    $fecha_inicial = strtotime($fecha_inicial);
+    $fecha_final = strtotime($fecha_final) + (24 * 60 * 60 - 1); // Incluir todo el día final.
+
+    // Obtener los tipos de contenido seleccionados desde la configuración.
+    $config = $this->config('estadisticas_rcn.settings');
+    $content_types = $config->get('content_types');
+
+    // Filtrar los nodos por tipo de contenido y rango de fechas.
+    $nids = \Drupal::entityQuery('node')
+      ->condition('status', 1)
+      ->condition('type', array_keys(array_filter($content_types)), 'IN')
+      ->condition('created', [$fecha_inicial, $fecha_final], 'BETWEEN')
+      ->sort('created', 'DESC')
+      ->execute();
+
+    $nodes = Node::loadMultiple($nids);
+
+    // Verifica si hay nodos
+    if (empty($nodes)) {
+      $build[] = [
+        '#markup' => $this->t('No hay contenidos creados entre las fechas seleccionadas.'),
+      ];
+      return $build;
+    }
+
+    foreach ($nodes as $node) {
+      // Construir el render array 
+      $build[] = [
+        '#markup' => $node->label() . ' (' . $node->id() . ')',
+      ];
+    }
+
+    return $build;
   }
 
 }
---
+++ src/Form/EstadisticasRcnFilterForm.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Drupal\estadisticas_rcn\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\node\Entity\NodeType;
+
+class EstadisticasRcnFilterForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'estadisticas_rcn_filter_form';
+  }
+
+  /**
+   * Build the form.
+   *
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $config = $this->config('estadisticas_rcn.settings');
+
+    $form['fecha_inicial'] = [
+      '#type' => 'date',
+      '#title' => $this->t('Fecha Inicial'),
+      '#default_value' => $config->get('fecha_inicial'),
+      '#required' => TRUE,
+    ];
+
+    $form['fecha_final'] = [
+      '#type' => 'date',
+      '#title' => $this->t('Fecha Final'),
+      '#default_value' => $config->get('fecha_final'),
+      '#required' => TRUE,
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Aplicar filtro'),
+    ];
+
+    return $form;
+    // return parent::buildForm($form, $form_state);
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Obtener la configuración editable.
+    $config = \Drupal::configFactory()->getEditable('estadisticas_rcn.settings');
+
+    // Guardar los valores en la configuración.
+    $config->set('fecha_inicial', $form_state->getValue('fecha_inicial'))
+           ->set('fecha_final', $form_state->getValue('fecha_final'))
+           ->save();
+
+    // Mensaje para el usuario.
+    \Drupal::messenger()->addMessage($this->t('El filtro ha sido aplicado con las fechas: @fecha_inicial a @fecha_final', [
+      '@fecha_inicial' => $form_state->getValue('fecha_inicial'), 
+      '@fecha_final' => $form_state->getValue('fecha_final')
+    ]));
+
+    // Redirección.
+    // $form_state->setRedirect('<ruta_de_destino>');
+  }
+  
+
+}
