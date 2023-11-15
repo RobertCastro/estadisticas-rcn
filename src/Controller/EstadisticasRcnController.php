bab7922c90bbe026419eb2602e47ac57da0621ef
Robert Castro <oneroberth@gmail.com>
2023-11-15 14:45:04 -0500

    Export

--- src/Controller/EstadisticasRcnController.php
+++ src/Controller/EstadisticasRcnController.php
@@ -2,8 +2,11 @@
 
 namespace Drupal\estadisticas_rcn\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\taxonomy\Entity\Term;
 use Drupal\node\Entity\Node;
+use Drupal\Core\Url;
 
 /**
  * Returns responses for Estadísticas RCN routes.
@@ -33,27 +36,90 @@ class EstadisticasRcnController extends ControllerBase {
   public function contenidosPorFecha() {
 
     $build = [];
-    // Incluir el formulario de fechas
     $build['form'] = \Drupal::formBuilder()->getForm('Drupal\estadisticas_rcn\Form\EstadisticasRcnFilterForm');
-    // Obtener los tipos de contenido seleccionados desde la configuración.
     $config = $this->config('estadisticas_rcn.settings');
 
-    // $configFilter = $this->config('estadisticas_rcn.settings');
     $fecha_inicial = $config->get('fecha_inicial');
     $fecha_final = $config->get('fecha_final');
     $default_base_url = $config->get('base_url');
 
-    // Convertir las fechas a formato UNIX timestamp.
     $fecha_inicial = strtotime($fecha_inicial);
-    $fecha_final = strtotime($fecha_final) + (24 * 60 * 60 - 1); // Incluir todo el día final.
-
+    $fecha_final = strtotime($fecha_final) + (24 * 60 * 60 - 1);
     $content_types = $config->get('content_types');
 
-    // Filtrar los nodos por tipo de contenido y rango de fechas.
+
     $nids = \Drupal::entityQuery('node')
       ->condition('status', 1)
       ->condition('type', array_keys(array_filter($content_types)), 'IN')
       ->condition('created', [$fecha_inicial, $fecha_final], 'BETWEEN')
+      ->range(0,10)
+      ->sort('created', 'DESC')
+      ->execute();
+
+    $build['btn_export'] = [
+      '#type' => 'link',
+      '#title' => $this->t('Exportar CSV'),
+      '#url' => Url::fromRoute('estadisticas_rcn.exportcsv'),
+      '#attributes' => ['class' => ['button', 'button--action', 'button--primary' ]],
+    ];
+
+    $nodes = Node::loadMultiple($nids);
+
+    if (empty($nodes)) {
+      $build[] = [
+        '#markup' => $this->t('No hay contenidos creados entre las fechas seleccionadas.'),
+      ];
+      return $build;
+    }
+
+    $items = [];
+    foreach ($nodes as $node) {
+
+      if ($node->hasField('field_seccion') && !$node->get('field_seccion')->isEmpty()) {
+          $section_entity = $node->field_seccion->entity;
+          $section_label = $section_entity ? $section_entity->getName() : $this->t('No especificado');
+      } else {
+          $section_label = $this->t('No especificado');
+      }
+      $has_video = isset($node->field_nota_con_video) ? $node->field_nota_con_video->value : FALSE;
+
+      $items[] = [
+        'title' => $node->label(),
+        'date' => $node->getCreatedTime(),
+        'author' => $node->getOwner()->getDisplayName(),
+        'section' => $section_label,
+        'has_video' => $has_video,
+        'link' => $node->toUrl()->toString(),
+        'content_type' => $node->bundle(),
+      ];
+    }
+
+    $build['content_table'] = [
+      '#theme' => 'estadisticas_rcn_contenidos',
+      '#items' => $items,
+      '#baseurl' => $default_base_url
+    ];
+
+    return $build;
+  }
+
+  public function ExportCSV() {
+    
+    $build = [];
+    $config = $this->config('estadisticas_rcn.settings');
+    $fecha_inicial = $config->get('fecha_inicial');
+    $fecha_final = $config->get('fecha_final');
+    $default_base_url = $config->get('base_url');
+    $content_types = $config->get('content_types');
+
+    $fecha_inicial = strtotime($fecha_inicial);
+    $fecha_final = strtotime($fecha_final) + (24 * 60 * 60 - 1);
+
+    $nids = \Drupal::entityQuery('node')
+      ->condition('status', 1)
+      ->condition('type', array_keys(array_filter($content_types)), 'IN')
+      ->condition('created', [$fecha_inicial, $fecha_final], 'BETWEEN')
+      ->range(0,10)
       ->sort('created', 'DESC')
       ->execute();
 
@@ -70,8 +136,12 @@ class EstadisticasRcnController extends ControllerBase {
     $items = [];
     foreach ($nodes as $node) {
 
-      $section_entity = $node->field_section->entity;
-      $section_label = $section_entity ? $section_entity->name() : $this->t('No especificado');
+      if ($node->hasField('field_seccion') && !$node->get('field_seccion')->isEmpty()) {
+          $section_entity = $node->field_seccion->entity;
+          $section_label = $section_entity ? $section_entity->getName() : $this->t('No especificado');
+      } else {
+          $section_label = $this->t('No especificado');
+      }
       $has_video = isset($node->field_nota_con_video) ? $node->field_nota_con_video->value : FALSE;
 
       $items[] = [
@@ -85,14 +155,21 @@ class EstadisticasRcnController extends ControllerBase {
       ];
     }
 
-    // Agregar el array de items al build y especificar el template.
-    $build['content_table'] = [
-      '#theme' => 'estadisticas_rcn_contenidos',
-      '#items' => $items,
-      '#baseurl' => $default_base_url
-    ];
+    $csv_lines = [];
+    $csv_lines[] = '"Title","Date","Author","Section","Has Video","Link","Content Type"'; // Encabezados del CSV
 
-    return $build;
+    foreach ($items as $item) {
+        $csv_lines[] = '"' . implode('","', array_map('strval', $item)) . '"';
+    }
+
+    $csv_content = implode("\r\n", $csv_lines);
+
+    $response = new Response($csv_content);
+    $response->headers->set('Content-Type', 'text/csv');
+    $response->headers->set('Content-Disposition', 'attachment; filename="export.csv"');
+
+    return $response;
+
   }
 
 }
